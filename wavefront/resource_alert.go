209e7ed866a47dcc98db25c81f8c9f87401be059
Louis McCormack <louis@spaceapegames.com>
2019-06-24 10:33:08 +0100

    first stab

--- wavefront/resource_alert.go
+++ wavefront/resource_alert.go
@@ -23,14 +23,27 @@ func resourceAlert() *schema.Resource {
 				Type:     schema.TypeString,
 				Required: true,
 			},
+			"alert_type": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Default: wavefront.AlertTypeClassic,
+			},
 			"target": {
 				Type:     schema.TypeString,
 				Required: true,
 			},
 			"condition": {
 				Type:      schema.TypeString,
-				Required:  true,
+				Optional:  true,
 				StateFunc: trimSpaces,
+			},
+			"threshold_conditions": {
+				Type:      schema.TypeMap,
+				Optional:  true,
+			},
+			"threshold_targets": {
+				Type:      schema.TypeMap,
+				Optional:  true,
 			},
 			"additional_information": {
 				Type:      schema.TypeString,
@@ -56,7 +69,7 @@ func resourceAlert() *schema.Resource {
 			},
 			"severity": {
 				Type:     schema.TypeString,
-				Required: true,
+				Optional: true,
 			},
 			"tags": {
 				Type:     schema.TypeSet,
@@ -71,6 +84,46 @@ func trimSpaces(d interface{}) string {
 	return strings.TrimSpace(d.(string))
 }
 
+func mapToThresholdConditions(m map[string]string) (*wavefront.ThresholdConditions, error) {
+	var t wavefront.ThresholdConditions
+	for sev, ts := range m {
+		switch sev {
+		case "severe":
+			t.Severe = trimSpaces(ts)
+		case "warn":
+			t.Warn = trimSpaces(ts)
+		case "info":
+			t.Info = trimSpaces(ts)
+		case "smoke":
+			t.Smoke = trimSpaces(ts)
+		default:
+			return nil, fmt.Errorf("invalid severity: %s", sev)
+		}
+	}
+
+	return &t, nil
+}
+
+func mapToThresholdTargets(m map[string]string) (*wavefront.ThresholdTargets, error) {
+	var t wavefront.ThresholdTargets
+	for sev, ts := range m {
+		switch sev {
+		case "severe":
+			t.Severe = trimSpaces(ts)
+		case "warn":
+			t.Warn = trimSpaces(ts)
+		case "info":
+			t.Info = trimSpaces(ts)
+		case "smoke":
+			t.Smoke = trimSpaces(ts)
+		default:
+			return nil, fmt.Errorf("invalid severity: %s", sev)
+		}
+	}
+
+	return &t, nil
+}
+
 func resourceAlertCreate(d *schema.ResourceData, m interface{}) error {
 	alerts := m.(*wavefrontClient).client.Alerts()
 
@@ -79,17 +132,43 @@ func resourceAlertCreate(d *schema.ResourceData, m interface{}) error {
 		tags = append(tags, tag.(string))
 	}
 
+	// Do some validation here? That we either have a condition or threshold_condition
+	// Also need to set the type accordinglt
+
 	a := &wavefront.Alert{
 		Name:                               d.Get("name").(string),
-		Target:                             d.Get("target").(string),
-		Condition:                          trimSpaces(d.Get("condition").(string)),
 		AdditionalInfo:                     trimSpaces(d.Get("additional_information").(string)),
 		DisplayExpression:                  trimSpaces(d.Get("display_expression").(string)),
 		Minutes:                            d.Get("minutes").(int),
 		ResolveAfterMinutes:                d.Get("resolve_after_minutes").(int),
 		NotificationResendFrequencyMinutes: d.Get("notification_resend_frequency_minutes").(int),
-		Severity:                           d.Get("severity").(string),
 		Tags:                               tags,
+	}
+
+	if d.Get("alert_type") == wavefront.AlertTypeThreshold {   //DO THIS!! YOUVE CHANGED IT
+		a.Type = wavefront.AlertTypeThreshold
+		conditions, err := mapToThresholdConditions(d.Get("threshold_conditions").(map[string]string))
+		if err != nil {
+			return err
+		}
+		a.ThresholdConditions = *conditions
+
+		targets, err := mapToThresholdTargets(d.Get("threshold_conditions").(map[string]string))
+		if err != nil {
+			return err
+		}
+		a.ThresholdTargets = *targets
+	} else if d.Get("condition") != nil {
+		// This is a CLASSIC alert
+		a.Type = wavefront.AlertTypeClassic
+		a.Condition = trimSpaces(d.Get("condition").(string))
+		if d.Get("severity") == nil {
+			return fmt.Errorf("severity must be supplied with a classic alert")
+		}
+		a.Severity =  d.Get("severity").(string)
+		a.Target =  d.Get("target").(string)
+	} else {
+		return fmt.Errorf("either condition or threshold_conditions is required")
 	}
 
 	// Create the alert on Wavefront
@@ -129,6 +208,7 @@ func resourceAlertRead(d *schema.ResourceData, m interface{}) error {
 	d.Set("notification_resend_frequency_minutes", tmpAlert.NotificationResendFrequencyMinutes)
 	d.Set("severity", tmpAlert.Severity)
 	d.Set("tags", tmpAlert.Tags)
+	d.Set("threshold_conditions", tmpAlert.ThresholdConditions)
 
 	return nil
 }
