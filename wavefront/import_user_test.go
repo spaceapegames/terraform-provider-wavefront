1cfc4e36d45b6eb0b91e8ff61621e637bced7bea
Mike McMahon <mike.mcmahon@wavefront.com>
2020-01-13 13:17:55 -0800

    adding import tests for user

--- wavefront/import_user_test.go
+++ wavefront/import_user_test.go
@@ -1,1 +1,43 @@
 package wavefront_plugin
+
+import (
+	"fmt"
+	"github.com/WavefrontHQ/go-wavefront-management-api"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"testing"
+)
+
+func TestAccUser_importBasic(t *testing.T) {
+	resourceName := "wavefront_user.basic"
+	var record wavefront.User
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckWavefrontUserDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckWavefrontUserImporter_basic(),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckWavefrontUserExists("wavefront_user.basic", &record),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func testAccCheckWavefrontUserImporter_basic() string {
+	return fmt.Sprintf(`
+resource "wavefront_user" "basic" {
+	email  = "test+tftesting@example.com"
+	groups = [
+		"agent_management",
+		"alerts_management",
+	]
+}`)
+}
