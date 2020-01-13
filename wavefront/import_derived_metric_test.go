716f4e9d3c4711fed83e3f906d028c62a007d6cf
Mike McMahon <mike.mcmahon@wavefront.com>
2020-01-13 13:24:27 -0800

    adding import tests for derived metrics

--- wavefront/import_derived_metric_test.go
+++ wavefront/import_derived_metric_test.go
@@ -1,1 +1,48 @@
 package wavefront_plugin
+
+import (
+	"fmt"
+	"github.com/WavefrontHQ/go-wavefront-management-api"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"testing"
+)
+
+func TestAccDerivedMetric_importBasic(t *testing.T) {
+	resourceName := "wavefront_derived_metric.derived"
+	var record wavefront.DerivedMetric
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckWavefrontDerivedMetricDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckWavefrontDerivedMetricImporter_Basic(),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckWavefrontDerivedMetricExists("wavefront_derived_metric.derived", &record),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func testAccCheckWavefrontDerivedMetricImporter_Basic() string {
+	return fmt.Sprintf(`
+resource "wavefront_derived_metric" "derived" {
+  name                   = "dummy derived metric"
+  minutes                = 5
+  query                  = "aliasMetric(5, \"some.metric\")"
+  additional_information = "this is a dummy derived metric"
+  tags = [
+    "somemetric",
+    "thatistagged",
+    "withmytags"
+  ]
+}
+`)
+}
